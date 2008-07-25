bb9abb3f67f133cb334d5a62094a28171b147822
Dianne Cook <dicook@iastate.edu>
2008-07-25 20:22:08 +0000

    density tour complete

--- R/display.r
+++ R/display.r
@@ -81,8 +81,6 @@ pcp_tour <- function(data, tourf = grand_tour, d = 2, aps = 1, fps = 30, ...) {
   tourf(start, velocity = aps / fps, step_fun = step, target_fun = target, total_steps = Inf, ..., data=data)
 }
 
-
-
 ggobi_tour <- function(data, tour = grand_tour, aps = 1, fps = 100, ...) {
   if(!require("rggobi", quiet = TRUE)) {
     stop("rggobi required for ggobi based tour")
@@ -108,10 +106,11 @@ ggobi_tour <- function(data, tour = grand_tour, aps = 1, fps = 100, ...) {
 }
 
 #density_tour(mtcars[, 1:5])
-density_tour<-function(data, tourf = grand_tour, d = 1, aps = 1, fps = 30, ...) {
+#density_tour(mtcars[, 1:5],method="density")
+density_tour<-function(data, tourf = grand_tour, method="hist", d = 1, aps = 1, fps = 30, ...) {
   # Standardise data
   data <- apply(data, 2, function(x) (x - min(x)) / diff(range(x)))
-  #labels <- abbreviate(colnames(data), 2)
+  labels <- abbreviate(colnames(data), 2)
   
   # Start with plot of first two variables
   start <- matrix(0, nrow = ncol(data), ncol = d)
@@ -120,21 +119,62 @@ density_tour<-function(data, tourf = grand_tour, d = 1, aps = 1, fps = 30, ...) 
   # Display 
   range <- c(-2, 2)
   par(pch = "s")
-  plot(NA, NA, xlim=range, ylim=range, xlab="", ylab="", axes=FALSE, frame=TRUE, xaxs="i", yaxs="i")
+  plot(NA, NA, xlim=range, ylim=c(-1.1,4), xlab="Data Projection",
+       ylab="Density", frame=TRUE, xaxs="i", yaxs="i")
   step <- function(step, proj) {
     Sys.sleep(1 / fps)
     
-    rect(-1.99, -1.99, 1.99, 1.99, col="#FFFFFFE6", border=NA)
+    rect(-1.99, -1.99, 1.99, 5, col="grey80", border=NA)
+                                        #col="#FFFFFFE6", border=NA)
+    abline(h=seq(0.5,3.5,by=0.5),col="white")
+    lines(c(0,0),c(-1,0),col="white")
+    lines(c(-1,-1),c(-1,0),col="white")
+    lines(c(1,1),c(-1,0),col="white")
+    
+    # Draw projected points
+    if (method=="hist") {
+      x<-hist(data%*%proj, breaks=seq(-2,2,0.2), plot=F)
+      rect(x$mids-0.1,rep(0,length(x$mids)),x$mids+0.1,x$density,col="black")
+    }
+    else if (method=="density") {
+      polygon(density(data%*%proj),lwd=2,col="black")
+      abline(h=0)
+    }
     
     # Draw tour axes
-    #segments(0, 0, proj[, 1], proj[, 2], col="grey50")
-    #theta <- seq(0, 2 * pi, length = 50)
-    #lines(cos(theta), sin(theta), col="grey50")
-    #text(proj, label = labels, col="grey50")
+    for (i in 1:length(proj)) {
+      x<-i/length(proj)
+      lines(c(0,proj[i]),c(-x,-x),col="black",lwd=3)
+      text(1,-x,labels[i],pos=4)
+    }
+  }
+  target <- function(target) {
+    rect(-1.99, -1.99, 1.99, 5, col="#7F7F7F33", border=NA)
+  }
 
+  cat("Press Ctrl+C to stop tour running\n")
+  tourf(start, velocity = aps / fps, step_fun = step, target_fun = target, total_steps = Inf, ..., data=data)
+}
+
+scatmat_tour<-function(data, tourf = grand_tour, d = 1, aps = 1, fps = 30, ...) {}
+
+image_tour<-function(data, tourf = grand_tour, d = 1, aps = 1, fps = 30, ...) {
+  # Standardise data
+#  data <- apply(data, 2, function(x) (x - min(x)) / diff(range(x)))
+#  labels <- abbreviate(colnames(data), 2)
+  start <- matrix(0, nrow=nrow(data[,,1])*ncol(data[,,1]), ncol = d)
+  diag(start) <- 1
+  
+  # Display 
+  plot(NA, NA,xlim=c(1,nrow(data[,,1])), ylim=c(1,ncol(data[,,1])), xlab="", ylab="", axes=FALSE, frame=TRUE, xaxs="i", yaxs="i")
+  
+  step <- function(step, proj) {
+    Sys.sleep(1 / fps)
+    
+    #rect(-1.99, -1.99, 1.99, 1.99, col="#FFFFFFE6", border=NA)
+    
     # Draw projected points
-    #points(data %*% proj, pch=20)
-    hist(data%*%proj, breaks=seq(-2,2,0.1), col="black")
+    image(data[,,1]*proj[1]+data[,,2]*proj[2]+data[,,3]*proj[3])
   }
   target <- function(target) {
     rect(-1.99, -1.99, 1.99, 1.99, col="#7F7F7F33", border=NA)
@@ -143,10 +183,6 @@ density_tour<-function(data, tourf = grand_tour, d = 1, aps = 1, fps = 30, ...) 
   cat("Press Ctrl+C to stop tour runnning\n")
   tourf(start, velocity = aps / fps, step_fun = step, target_fun = target, total_steps = Inf, ..., data=data)
 }
-
-scatmat_tour<-function(data, tourf = grand_tour, d = 2, aps = 1, fps = 30, ...) {}
-
-image_tour<-function(data, tourf = grand_tour, d = 2, aps = 1, fps = 30, ...) {}
 
 ts_tour<-function(data, tourf = grand_tour, d = 2, aps = 1, fps = 30, ...) {}
 
