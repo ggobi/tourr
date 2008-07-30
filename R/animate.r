ad6cef3b79b44ee014c12d4f1b8947fccae4723b
Hadley Wickham <hadley@rice.edu>
2008-07-30 16:01:24 +0000

    Make it possible to save animations to disk

--- R/animate.r
+++ R/animate.r
@@ -1,5 +1,5 @@
 # Also needs to be able to save to disk
-animate <- function(data, tourf, d, aps = 1, fps = 30, start = NULL, render_frame, render_target, render_data, render_transition, ..., rescale = TRUE, sphere = FALSE) {
+animate <- function(data, tourf, d, aps = 1, fps = 30, start = NULL, render_frame, render_target, render_data, render_transition, ..., rescale = TRUE, sphere = FALSE, file = NULL, dev = NULL, dev.settings = list()) {
   if (rescale) data <- rescale(data)
   if (sphere) data  <- sphere(data)
   
@@ -8,12 +8,23 @@ animate <- function(data, tourf, d, aps = 1, fps = 30, start = NULL, render_fram
     diag(start) <- 1    
   }
   
-  render_frame()
-  step <- function(step, proj) {
-    Sys.sleep(1 / fps)
-    render_transition()
-    render_data(data, proj)
-  }  
+  if (is.null(file)) {
+    render_frame()
+    step <- function(step, proj) {
+      Sys.sleep(1 / fps)
+      render_transition()
+      render_data(data, proj)
+    }  
+    
+  } else {
+    do.call(dev, c(list(file = file), dev.settings))
+    on.exit(dev.off())
+
+    step <- function(step, proj) {
+      render_frame()
+      render_data(data, proj)
+    }
+  }
   
   cat("Press Ctrl+C to stop tour runnning\n")
   tourf(
@@ -22,6 +33,8 @@ animate <- function(data, tourf, d, aps = 1, fps = 30, start = NULL, render_fram
     total_steps = Inf, ..., data = data
   )
 }
+
+
 
 rescale <- function(df) {
   apply(df, 2, function(x) (x - min(x)) / diff(range(x)))
