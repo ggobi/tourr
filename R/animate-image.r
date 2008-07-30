5f20e9ea6b3123405eaddaf9f5ba8a876a1e46d3
Hadley Wickham <hadley@rice.edu>
2008-07-30 15:28:06 +0000

    Faster image tour

--- R/animate-image.r
+++ R/animate-image.r
@@ -1,25 +1,34 @@
-# image_tour(pict_samp)
-# This is too slow
-animate_image<-function(data, tourf = grand_tour, ...) {
+# Image tour uses a 3d array:
+#   * first two dimensions are location
+#   * third is variables
+#
+#X animate_image(ozone)
+animate_image <- function(data, tourf = grand_tour, ...) {
+  
   xs <- dim(data)[1]
   ys <- dim(data)[2]
-  
-  start <- matrix(0, nrow = dim(data)[3], ncol = 1)
-  diag(start) <- 1
-  
+  zs <- dim(data)[3]
+
+  # Collapse 3d array into 2d matrix with 
+  # rows and columns in first dimension
+  dim(data) <- c(xs * ys, zs)
+
   render_frame <- function() { 
     blank_plot(xlim = c(1, xs), ylim = c(1, xs))
   }
   
   render_data <- function(data, proj) {
+    z <- data %*% proj
+    dim(z) <- c(xs, ys)
     image(
-      data[,,1]*proj[1]+data[,,2]*proj[2]+data[,,3]*proj[3], 
+      x = seq_len(xs), y = seq_len(ys), 
+      z = t(z), 
       zlim = c(-2, 2), add = TRUE
     )
   }
 
   animate(
-    data = data, tourf = tourf, start = start, 
+    data = data, tourf = tourf, d = 1, 
     render_frame = render_frame, render_data = render_data,
     render_transition = nul, render_target = nul, 
     ...
