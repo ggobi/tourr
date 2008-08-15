c5ab8af907d08175bd463de29c773f20d260134c
Hadley Wickham <hadley@rice.edu>
2008-08-15 19:18:42 +0000

    Tweak colours for red-blue glasses

--- R/animate-stereo.r
+++ R/animate-stereo.r
@@ -1,3 +1,7 @@
+blue <- rgb(0, 0.91, 0.89)
+red <- rgb(0.98, 0.052, 0)
+green <- "green2"
+
 project3d <- function(d3, length = par("din")[1] * 25.4, z0 = 300, d = 30) {
   length <- length * 0.3
   x <- d3[, 1] * length
@@ -18,8 +22,8 @@ project3d <- function(d3, length = par("din")[1] * 25.4, z0 = 300, d = 30) {
 anaglyph <- function(d3, ...) {
   d2 <- project3d(d3)
 
-  with(d2, points(right, y, col="green2")) # skyblue
-  with(d2, points(left, y, col="red"))
+  with(d2, points(right, y, col=blue, pch = 20)) # skyblue
+  with(d2, points(left, y, col=red, pch=20))
 }
 
 # animate_stereo(flea[, 1:6])
@@ -27,21 +31,21 @@ animate_stereo <- function(data, tourf = grand_tour, ...) {
   labels <- abbreviate(colnames(data), 2)
   
   render_frame <- function() {
-    par(pty = "s", bg = "grey80")
+    par(pty = "s", bg = "grey85")
     blank_plot(xlim = c(-1, 1), ylim = c(-1, 1))    
   }
   render_transition <- function() {
-    rect(-1, -1, 1, 1, col="#CCCCCCE6", border=NA)
+    rect(-1, -1, 1, 1, col="#D9D9D9E6", border=NA)
   }
   render_data <- function(data, proj) {
     anaglyph(data %*% proj)
     
     axes <- project3d(proj)
     with(axes, {
-      segments(0, 0, right, y, col="green2")
-      segments(0, 0, left, y, col="red")
-      text(right, y, col="green2", label = labels)
-      text(left, y, col="red", label = labels)
+      segments(0, 0, right, y, col=blue)
+      segments(0, 0, left, y, col=red)
+      text(right, y, col=blue, label = labels)
+      text(left, y, col=red, label = labels)
     })
   }
 
@@ -52,4 +56,3 @@ animate_stereo <- function(data, tourf = grand_tour, ...) {
     ...
   )
 }
-
