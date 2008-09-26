4d6108136084a6c2a2286ae70547fbae1c011778
Hadley Wickham <hadley@rice.edu>
2008-09-26 18:27:08 +0000

    Remove interpolation from geodesic history

--- R/history-geodesics.r
+++ R/history-geodesics.r
@@ -1,4 +1,4 @@
-history_geodesics <- function(data, tour_f = grand_tour, d = 2, nbases = 100, interpolate = FALSE, ..., rescale = TRUE, sphere = FALSE){
+history_geodesics <- function(data, tour_f = grand_tour, d = 2, nbases = 100, ..., rescale = TRUE, sphere = FALSE){
   if (rescale) data <- rescale(data)
   if (sphere) data  <- sphere(data)
   
@@ -6,31 +6,24 @@ history_geodesics <- function(data, tour_f = grand_tour, d = 2, nbases = 100, in
   start <- basis_random(ncol(data), d)
 
   # A bit inefficient, but otherwise save changes to rest of tour code
-  velocity <- if (interpolate) 0.05 else 10
+  velocity <- 10
 
   projs <- array(NA, c(ncol(data), d, nbases + 1))
   projs[, , 1] <- start
   princ_dirs <- array(NA, c(ncol(data), d, nbases + 1))
   count <- 1
   
-  step <- function(step, proj, geodesic) {
-    if (interpolate) {
-      count <<- count+1
-      projs[, , count] <<- proj
-    }
-  }
   target <- function(target, geodesic) {
-    if (!interpolate) {
-      count <<- count+1
-      projs[, , count] <<- target      
-    }
+    count <<- count+1
+    projs[, , count] <<- target      
+
     princ_dirs[, , count] <<- geodesic$Gz
     cat(dim(princ_dirs[, , count]),"\n")
   }
 
   tour_f(
     start, velocity = velocity, total_steps = nbases + 1,
-    step_fun = step, target_fun = target,  ..., data=data
+    step_fun = nul, target_fun = target,  ..., data=data
   )
   
   # Remove empty matrices for tours that terminated early
