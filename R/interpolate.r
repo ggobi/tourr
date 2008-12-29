c9dca65ad3d6f5a664609dcd336ddc0ec3332c2f
Hadley Wickham <hadley@rice.edu>
2008-12-29 19:38:20 +0000

    More code improvements and documentation tweaks

--- R/interpolate.r
+++ R/interpolate.r
@@ -1,23 +1,33 @@
-# t1 <- save_history(flea[, 1:6], nbases = 10, d = 1)
-# tinterp <- interpolate(t1)
-
-# This function takes a set of bases and produces a tour by geodesically 
-# interpolating between each basis
-interpolate <- function(basis_set, velocity = 0.05) {
+#' Interpolate geodesically between bases
+#' 
+#' This function takes a set of bases and produces a tour by geodesically 
+#' interpolating between each basis
+#' 
+#' @param basis_set input basis set
+#' @param angle target distance (in radians) between bases
+#' @keyword hplot
+#' @example
+#' t1 <- save_history(flea[, 1:6], grand_tour(1), max = 10)
+#' dim(t1)
+#' dim(interpolate(t1, 0.01))
+#' dim(interpolate(t1, 0.05))
+#' dim(interpolate(t1, 0.1))
+interpolate <- function(basis_set, angle = 0.05) {
   basis_set <- as.list(basis_set)
-  get_basis <- function(i) basis_set[[i]]
-  n <- length(basis_set)
 
+  n <- length(basis_set)
   if (n < 2) return(basis_set)
+
   output <- list()
   
+  get_basis <- function(i) basis_set[[i]]
   path <- geodesic(get_basis(1), get_basis(2))
   dist <- sqrt(sum(path$tau ^ 2))
 
-  step <- 0
-  nsteps <- ceiling(dist / velocity)
-
   i <- 2
+  step <- 0
+  nsteps <- ceiling(dist / angle)
+
   while(i < n | step < nsteps) {
     proj <- step_fraction(path, step / nsteps)
     output <- append(output, list(proj))
@@ -28,7 +38,7 @@ interpolate <- function(basis_set, velocity = 0.05) {
       dist <- sqrt(sum(path$tau ^ 2))
 
       step <- 0
-      nsteps <- ceiling(dist / velocity)
+      nsteps <- ceiling(dist / angle)
     }
     step <- step + 1
   }  
--- R/tour-grand.r
+++ R/tour-grand.r
@@ -10,6 +10,19 @@
 #' animate_xy(flea[, 1:6])
 #' animate_pcp(flea[, 1:6])
 #' animate_pcp(flea[, 1:6], grand_tour(4))
+#'
+#' # The grand tour is a function:
+#' tour2d <- grand_tour(2)
+#' is.function(tour2d)
+#' 
+#' # with two parameters, the previous projection and the data set
+#' args(tour2d)
+#' # if the previous projection is null, it will generate a starting
+#' # basis, otherwise the argument is ignored
+#' tour2d(NULL, mtcars)
+#' # the data argument is just used to determine the correct dimensionality
+#' # of the output matrix
+#' tour2d(matrix(), mtcars[, 1:2])
 grand_tour <- function(d = 2) {
   generator <- function(current, data) {
     if (is.null(current)) return(basis_init(ncol(data), d))
