6729b8a5de6031fedb926f5f7e9ddd491f7ecaad
Hadley Wickham <hadley@rice.edu>
2009-01-15 14:17:40 +0000

    Generalise correlation tour to nd

--- R/tour-correlation.r
+++ R/tour-correlation.r
@@ -1,14 +1,31 @@
-# pos is a logical vector - T corresponds to X, F corresponds to Y
+#' The independent tour
+#' 
+#' The independent tour combines a set of independent 1d tours to produce
+#' a nd tour.  For the special case of 2d, this is known as a correlation 
+#' tour
+#'
+#' @param pos a numeric vector describing which variables are mapped to
+#'   which dimensions: 1 corresponds to first, 2 to second etc.
+#' examples
+#'
+#' animate_xy(flea[, 1:3], independent_tour(c(1, 2, 2)))
+#' animate_xy(flea[, 1:4], independent_tour(c(1, 2, 1, 2)))
+#' animate_pcp(flea[, 1:6], independent_tour(c(1, 2, 3, 2, 1, 3)))
 
-correlation_tour <- function(pos) {
-  x <- rep(0, length(pos))
-  y <- rep(0, length(pos))
-  
-  generator <- function(current) {
-    x[pos] <<- basis_random(sum(pos), 1)
-    y[!pos] <<- basis_random(sum(!pos), 1)
+independent_tour <- function(pos) {
+  stopifnot(is.numeric(pos))
+  stopifnot(all.equal(pos, trunc(pos)))
+
+  d <- max(pos)
+  generator <- function(current, data) {
+    if (is.null(current)) return(basis_init(ncol(data), d))
     
-    cbind(x, y)
+    mat <- matrix(0, ncol = d, nrow = length(pos))
+    for(i in seq_len(d)) {
+      mat[pos == i, i] <- basis_random(sum(pos == i), 1)
+    }
+    
+    mat
   }
 
   new_tour_path("correlation", generator)
