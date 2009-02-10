5ccb002c86839b487da26535f1d96e96f003cb20
Hadley Wickham <hadley@rice.edu>
2009-02-10 16:51:50 +0000

    Choose better limits for animate_xy, and allow user to override

--- R/animate-xy.r
+++ R/animate-xy.r
@@ -8,6 +8,8 @@
 #'  center of data cloud and make it easier to focus on the changing shape
 #'  rather than position.
 #' @param axes position of the axes: center, bottomleft or off
+#' @param limit limits of the projected data.  Defaults to 3 * square root
+#'  of the largest eigenvalue.
 #' @param ... other arguments passed on to \code{\link{animate}}
 #'
 #' @examples
@@ -21,31 +23,40 @@
 #' animate_xy(flea[, 1:6], axes = "off")
 #' animate_xy(flea[, 1:6], independent_tour(c(1, 2, 1, 2, 1, 2)),
 #'   axes = "bottomleft")
-animate_xy <- function(data, tour_path = grand_tour(), center = TRUE, axes = "center", ...) {
+animate_xy <- function(data, tour_path = grand_tour(), center = TRUE, axes = "center", limit = NULL, ...) {
   labels <- abbreviate(colnames(data), 2)
   axes <- match.arg(axes, c("center", "bottomleft", "off"))
   
-  render_frame <- function() {
+  render_frame <- function(rng) {
     par(pty = "s", mar = rep(1,4))
-    blank_plot(xlim = c(-2, 2), ylim = c(-2, 2))    
+    # 
   }
   render_transition <- function() {
     # rect(-1.99, -1.99, 1.99, 1.99, col="#FFFFFFE6", border=NA)
   }
   render_data <- function(data, proj, geodesic) {
-    render_frame()
+    if (is.null(limit)) {
+      first_eigen <- sqrt(eigen(var(data))$values[1])
+      limit <- 3 * first_eigen
+    }
+    rng <- c(-limit, limit)      
+    blank_plot(xlim = rng, ylim = rng)
+    
     # Render axes
     if (axes == "center") {
-      segments(0, 0, proj[, 1], proj[, 2], col="grey50")
-      theta <- seq(0, 2 * pi, length = 50)
-      lines(cos(theta), sin(theta), col="grey50")
-      text(proj, label = labels, col="grey50")
+      axis_scale <- 2 * limit / 3
+      axis_pos <- 0
     } else if (axes == "bottomleft") {
-      segments(-1.25, -1.25, -1.25+proj[, 1]/2, -1.25+proj[, 2]/2, col="grey50")
-      theta <- seq(0, 2 * pi, length = 50)
-      lines(-1.25+cos(theta)/2, -1.25+sin(theta)/2, col="grey50")
-      text(-1.25+proj[, 1]/2, -1.25+proj[, 2]/2, label = labels, col="grey50")
+      axis_scale <- limit / 6
+      axis_pos <- -2/3 * limit
     }
+    
+    adj <- function(x) axis_pos + x * axis_scale
+
+    segments(adj(0), adj(0), adj(proj[, 1]), adj(proj[, 2]), col="grey50")
+    theta <- seq(0, 2 * pi, length = 50)
+    lines(adj(cos(theta)), adj(sin(theta)), col = "grey50")
+    text(adj(proj[, 1]), adj(proj[, 2]), label = labels, col = "grey50")
 
     g <- geodesic$Gz
     
