2ea57a01f0674bc791c90f0386b665230230293a
Bei Huang <beihuang@iastate.edu>
2009-02-27 20:18:51 +0000

    One change in interesting-indices.r Changed fit <- mnova(as.matrix(mat) ~ cl)

--- R/interesting-indices.r
+++ R/interesting-indices.r
@@ -16,8 +16,10 @@ cm <- function(mat) 1 - holes(mat)
 lda_pp <- function(mat, cl) {
   if (length(unique(cl)) < 2)
     return(NA)
-
-  fit <- manova(mat ~ cl)
+#########################################change to as.matrix
+  fit <- manova(as.matrix(mat) ~ cl)                      
 
   1 - summary(fit,test="Wilks")$stats[[3]]
 }
+
+
