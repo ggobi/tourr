6e87c11174503b638090085e79357b84a9093cd8
Barret Schloerke <barret@barretschloerke.com>
2009-09-28 05:33:26 +0000

    version sent to CRAN.  removed pda_pp function and .Rd file.

--- R/interesting-indices.r
+++ R/interesting-indices.r
@@ -48,22 +48,3 @@ lda_pp <- function(cl) {
   }
 }
 
-#' PDA projection pursuit index.
-#'
-#' Calculates the PDA projection pursuit index. See Lee and Cook (2009)
-#' A Projection Pursuit Index for Large p Small n Data,
-#' Statistics and Computing
-#'
-#' @param cl class to be used.  Such as "color"
-#' @param lambda parameter used the "interesting" persuit.  It ranges from 0 to 1. O is lda_pp.  pda_pp is an updated form of lda_pp.
-#' @keywords hplot
-pda_pp <- function(cl, lambda) {
-  if (length(unique(cl)) < 2) 
-    stop("PDA index needs at least two classes")
-
-  # require(classPP)
-  function(mat){
-    # PPindex.class("PDA",mat,cl,lambda=lambda)
-    PPindex.PDA1(mat,cl,lambda)
-  }
-}
