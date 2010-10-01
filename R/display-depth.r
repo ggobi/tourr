6b1f83f565a4e771b07a6651dc84082e85625dda
Hadley Wickham <hadley@rice.edu>
2010-10-01 17:20:28 +0000

    Better scaling for display_depth

--- R/display-depth.r
+++ R/display-depth.r
@@ -2,8 +2,12 @@
 #' 
 #' Suggestion to use gray background and colour saturation (instead of 
 #' gray shading) by Graham Wills.
-#' Animate a 3d projection with depth cues.
-#'
+# 
+#' @param center should projected data be centered to have mean zero (default:
+#'   TRUE). This pins the centre of the data to the same place, and makes it
+#'   easier to focus on the shape.
+#' @param half_range half range to use when calculating limits of projected.
+#'   If not set, defaults to maximum distance from origin to each row of data.
 #' @param ... other arguments passed on to \code{\link{animate}}
 #' @seealso \code{\link{animate}} for options that apply to all animations
 #' @keywords hplot
@@ -11,35 +15,31 @@
 #' 
 #' @examples
 #' animate_depth(flea[, 1:6])
-display_depth <- function(...) {
+display_depth <- function(center = TRUE, half_range = NULL, ...) {
   shades <- hcl(240, 
     c = seq(0, 60, length = 100), 
     l = seq(80, 20, length = 100)
   )
   
-  rng <- limit <- NULL
   init <- function(data) {
-    if (is.null(limit)) {
-      data <- scale(data, center = TRUE, scale = FALSE)
-      limit <<- max(sqrt(colSums(data ^ 2)))
-    }
-    rng <<- c(-limit, limit)    
+    half_range <<- compute_half_range(half_range, data, center)
   }
   
   render_frame <- function() {
     par(pty = "s", mar = rep(1,4))
-    blank_plot(xlim = rng, ylim = rng)
+    blank_plot(xlim = c(-1, 1), ylim = c(-1, 1))
     render_transition()
   }
   render_transition <- function() {
-    rect(-limit, -limit, limit, limit, col="grey80", border=NA)
+    rect(-1, -1, 1, 1, col="grey80", border=NA)
   }
   render_data <- function(data, proj, geodesic) {
     x <- data %*% proj
-    x <- scale(x, center = TRUE, scale = FALSE)
+    if (center) x <- center(x)
+    x <- x / half_range
 
     depth <- x[, 3]
-    # depth ranges mostly between -1 and 1, 
+    # depth ranges between -1 and 1, 
     # so depth_std should lie between 0 and 1
     depth_std <- depth / 2 + 0.5 
     size <- 0.5 + depth_std * 3
