d5b84c2b634f71e16bbee96510594d9e15d188c8
hadley <h.wickham@gmail.com>
2014-04-22 17:03:39 -0500

    Remove rggobi from dependencies

--- R/path-dist.r
+++ R/path-dist.r
@@ -1,5 +1,5 @@
 #' Compute distance matrix from bases.
-#' 
+#'
 #' @param history history of the plots
 #' @export
 #' @examples
@@ -10,35 +10,27 @@
 #'  # Using MDS
 #'  d <- path_dist(grand)
 #'  ord <- as.data.frame(MASS::isoMDS(d)$points)
-#'  qplot(V1, V2, data = ord, geom="path") + 
+#'  qplot(V1, V2, data = ord, geom="path") +
 #'  coord_equal() + labs(x = NULL, y = NULL)
-#' 
-#'  
-#'  library(rggobi)
-#'  # Using a tour in ggobi
-#'  x<-t(rbind(grand[,1,])); colnames(x)<-c("a11","a21","a31","a41","a51","a61")
-#'  y<-t(rbind(grand[,2,])); colnames(x)<-c("a12","a22","a32","a42","a52","a62")
-#'  z<-cbind(x,y)
-#'  ggobi(z) 
 #' }
 #'
 #' # 5 guided tours  -----------------------------
 #' holes1d <- guided_tour(holes, 1)
-#' tries <- replicate(5, save_history(flea[, 1:6], holes1d, max = 10), 
+#' tries <- replicate(5, save_history(flea[, 1:6], holes1d, max = 10),
 #'   simplify = FALSE)
 #' tries2 <- lapply(tries, interpolate, 0.2)
-#' 
+#'
 #' bases <- unlist(lapply(tries2, as.list), recursive = FALSE)
 #' class(bases) <- "history_list"
 #' index_values <- paths_index(tries2, holes)
 #' d <- path_dist(bases)
 #' ord <- as.data.frame(cmdscale(d, 2))
-#' 
+#'
 #' info <- cbind(ord, index_values)
 #' if (require("ggplot2")) {
 #' qplot(step, value, data = info, geom="line", group = try)
-#' qplot(V1, V2, data = info, geom="path", group = try) + 
-#'   geom_point(aes(size = value)) + 
+#' qplot(V1, V2, data = info, geom="path", group = try) +
+#'   geom_point(aes(size = value)) +
 #'   coord_equal()
 #' last_plot() + facet_wrap(~ try)
 #' }
