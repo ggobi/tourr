fdaab8b88d24efff15a9896251c801d6c21f89f0
huizezhang-sherry <huizezhangsh@gmail.com>
2020-04-13 18:53:58 +1000

    update searching method for global structure

--- R/search-better.r
+++ R/search-better.r
@@ -14,19 +14,32 @@ basis_nearby <- function(current, alpha = 0.5, method = "linear") {
 #' Search for a better projection near the current projection.
 #' @keywords internal
 search_better <- function(current, alpha = 0.5, index, max.tries = Inf,
-  method = "linear", cur_index = NA
-) {
+  method = "linear", cur_index = NA) {
   if (is.na(cur_index)) cur_index <- index(current)
 
   cat("Old", cur_index, "\n")
   try <- 1
+
   while(try < max.tries) {
     new_basis <- basis_nearby(current, alpha, method)
-
     new_index <- index(new_basis)
+
+    record <- record %>% dplyr::add_row(basis = list(new_basis),
+                                          index_val = new_index,
+                                          info = "random_search",
+                                          tries = tries,
+                                          loop = try)
+
     if (new_index > cur_index) {
       cat("New", new_index, "try", try, "\n")
-      return(new_basis)
+      record <- record %>%
+        dplyr::mutate(row = row_number(),
+                      info = ifelse(row == max(row), "new_basis", info)) %>%
+        dplyr::select(-row)
+
+      target <- record %>% tail(1) %>% pull(basis)
+      return(list(record = record,
+                  target = target[[1]]))
     }
     try <- try + 1
   }
@@ -46,14 +59,38 @@ search_better_random <- function(current, alpha = 0.5, index,
   while(try < max.tries) {
     new_basis <- basis_nearby(current, alpha, method)
     new_index <- index(new_basis)
+
+    record <- record %>% dplyr::add_row(basis = list(new_basis),
+                                        index_val = new_index,
+                                        info = "random_search",
+                                        tries = tries,
+                                        loop = try)
+
     if (new_index > cur_index) {
       cat("New", new_index, "try", try, "\n")
-      return(new_basis)
+
+      record <- record %>%
+        dplyr::mutate(row = row_number(),
+                      info = ifelse(row == max(row), "new_basis", info)) %>%
+        dplyr::select(-row)
+
+      target <- record %>% tail(1) %>% pull(basis)
+      return(list(record = record,
+                  target = target[[1]]))
     }
     else if (abs(new_index-cur_index) < eps) {
+      cat("insert random step since abs(new_index-cur_index) < eps")
       new_basis <- basis_random(nrow(current), ncol(current))
-      cat("Adding random step", cur_index, new_index, "\n")
-      return(new_basis)
+
+      record <- record %>% dplyr::add_row(basis = list(new_basis),
+                                          index_val = new_index,
+                                          info = "random_search",
+                                          tries = tries,
+                                          loop = try)
+
+      target <- record %>% tail(1) %>% pull(basis)
+      return(list(record = record,
+                  target = target[[1]]))
     }
     try <- try + 1
   }
