8f3031a4beb56deccacb25094a1c7336fd45b053
dicook <visnut@gmail.com>
2020-10-05 15:59:33 +1100

    fixed little tour

--- R/animate.r
+++ R/animate.r
@@ -38,7 +38,7 @@
 #'
 #' \dontrun{animate(f, max_frames = 10, fps = 1, aps = 0.1)}
 animate <- function(data, tour_path = grand_tour(), display = display_xy(),
-                    start = NULL, aps = 1, fps = 15, max_frames = Inf,
+                    start = NULL, aps = 1, fps = 10, max_frames = Inf,
                     rescale = TRUE, sphere = FALSE, verbose = FALSE,...) {
 
   verbose <<- verbose
--- R/tour-little.r
+++ R/tour-little.r
@@ -24,6 +24,10 @@ little_tour <- function(d = 2) {
       little <<- bases_little(ncol(data), d)
     }
     step <<- (step %% length(little)) + 1
+    if (is.null(current)) {
+      # need an intercept to start the little tour
+      return(little[[step]])
+    }
     target <- little[[step]]
     list(target = target)
   }
@@ -39,5 +43,8 @@ little_tour <- function(d = 2) {
 bases_little <- function(p, d = 2) {
   b <- diag(rep(1, p))
   vars <- utils::combn(p, d)
+  # Let's make the list twice as long, so it visits pairs multiple
+  # times, and in different order
+  vars <- cbind(vars[,sample(1:ncol(vars))], vars[,sample(1:ncol(vars))])
   lapply(seq_len(ncol(vars)), function(i) b[, vars[, i]] )
 }
