ac305cafef12ed220bdf45ffcb5765a4d96d5ebd
dicook <visnut@gmail.com>
2022-07-12 21:31:49 +1000

    missing R code - wow

---
+++ R/tour-radial.r
@@ -0,0 +1,44 @@
+#' A radial tour path.
+#'
+#' The radial tour rotates a chosen variable axis out of the current
+#' projection.
+#'
+#' Usually, you will not call this function directly, but will pass it to
+#' a method that works with tour paths like \code{\link{animate}},
+#' \code{\link{save_history}} or \code{\link{render}}.
+#'
+#' @param start initial projection matrix
+#' @param var variable chosen to rotate out
+#' @export
+#' @examples
+#' animate_xy(flea[, 1:6], radial_tour(basis_random(6, 2), var = 4))
+#' animate_dist(flea[, 1:6], radial_tour(basis_random(6, 1), var = 4))
+#' animate_scatmat(flea[, 1:6], radial_tour(basis_random(6, 3), var = 4))
+radial_tour <- function(start, var = 1) {
+  first <- TRUE
+  out <- TRUE
+
+  generator <- function(current, data, ...) {
+    if (first) {
+      new_basis <- start
+      first <<- FALSE
+      return(new_basis)
+    }
+    if (out) {
+      new <- start
+      new[var,] <- rep(0, ncol(start))
+      new <- orthonormalise(new)
+      new_basis <- new
+      out <<- !out
+    }
+    else {
+      new_basis <- start
+      out <<- !out
+    }
+
+    target <- new_basis
+    list(target = target)
+  }
+
+  new_geodesic_path("radial", generator)
+}
